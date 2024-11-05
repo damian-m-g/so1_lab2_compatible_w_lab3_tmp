1278270be9b21162568fe1586337207405fc3bce
damian-m-g <gonzalezdamianm@gmail.com>
2024-11-05 15:26:01 -0300

    Initial commit
    - All things set to start.

---
+++ src/main.c
@@ -0,0 +1,21 @@
+/**
+ * @file main.c
+ * @brief Entry point of the system
+ */
+
+// STD headers
+#include <stdlib.h>
+
+// lib or deps headers
+#include <cjson/cJSON.h>
+
+//! \brief Main function of the program.
+int main(int argc, char* argv[])
+{
+
+    // Args no usados
+    (void)argc;
+    (void)argv;
+
+    return EXIT_SUCCESS;
+}
